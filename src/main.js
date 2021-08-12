ab0bd6a8cbe7e23c1bc5b5b51183796d2eaa01ca
commit <sfpannda@gmail.com>
2021-08-12 12:14:20 +0300

    иконки обновил

---
+++ src/main.js
@@ -0,0 +1,31 @@
+if ($(".like")) {
+    $(".like").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+    })
+}
+
+if ($(".like-product-str")) {
+    $(".like-product-str").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+    })
+}
+
+if ($(".like-bottom")) {
+    $(".like-bottom").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+    })
+}
+
+if ($(".block-img-video")) {
+    $(function() {
+        $(".block-img-video").on("click", ".link-play", function(e) {
+            e.preventDefault();
+            $(this).toggleClass("active");
+            $(".img-item", e.delegateTarget).toggleClass('active');
+            $(".video-item", e.delegateTarget).toggleClass('active');
+        })
+    });
+}
