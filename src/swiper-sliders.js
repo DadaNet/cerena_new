3a460a03965c3d7a174fcecc92596d2da95b2434
sfcpa <https://github.com/DadaNet/cerena_new.git>
2021-08-16 11:39:12 +0300

    вернул некоторый вид дял страницы итовара. сейчас толкьо слайдеры некрасивые.

--- src/swiper-sliders.js
+++ src/swiper-sliders.js
@@ -3,87 +3,108 @@ import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
 //slider str product -> photo product
-var swiperSmall = new Swiper(".slider-product-small", {
-    loop: true,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    slideToClickedSlide: true,
-    spaceBetween: 20,
-    initialSlide: 1,
+if ($('.slider-product-small')) {
+    var swiperSmall = new Swiper(".slider-product-small", {
+        loop: true,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        slideToClickedSlide: true,
+        spaceBetween: 20,
+        initialSlide: 1,
 
-    breakpoints: {
-        320: {
-            slidesPerView: 2,
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+            },
+            575: {
+                slidesPerView: 3,
+            },
         },
-        575: {
-            slidesPerView: 3,
+    });
+    var swiperBig = new Swiper(".slider-product-big", {
+        loop: true,
+        slideToClickedSlide: true,
+
+        thumbs: {
+            swiper: swiperSmall,
         },
-    },
-});
-var swiperBig = new Swiper(".slider-product-big", {
-    loop: true,
-    slideToClickedSlide: true,
-
-    thumbs: {
-        swiper: swiperSmall,
-    },
-});
+    });
+}
 
 
-var swiperSmall = new Swiper("#slider-comment", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    initialSlide: 1,
+if ($('#slider-comment')) {
+    var swiperSmall = new Swiper("#slider-comment", {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        initialSlide: 1,
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
 
-var swiperSmall = new Swiper(".slider-product-str", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    initialSlide: 1,
 
-    navigation: {
-        nextEl: ".swiper-button-next-two",
-        prevEl: ".swiper-button-prev-two",
-    },
-});
-var swiperSmall = new Swiper(".one-block-slider", {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    initialSlide: 1,
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-var swiperSmall = new Swiper(".block-slider-about", {
-    loop: true,
-    slidesPerView: 3,
-    initialSlide: 1,
+if ($('.slider-product-str')) {
+    var swiperSmall = new Swiper(".slider-product-str", {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        initialSlide: 1,
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-var swiperSmall = new Swiper(".main-block-slider", {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    initialSlide: 1,
+        navigation: {
+            nextEl: ".swiper-button-next-two",
+            prevEl: ".swiper-button-prev-two",
+        },
+    });
+}
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+
+
+if ($('.one-block-slider')) {
+    var swiperSmall = new Swiper(".one-block-slider", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        initialSlide: 1,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
+
+
+if ($('.block-slider-about')) {
+    var swiperSmall = new Swiper(".block-slider-about", {
+        loop: true,
+        slidesPerView: 3,
+        initialSlide: 1,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
+
+
+if ($('.main-block-slider')) {
+    var swiperSmall = new Swiper(".main-block-slider", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        initialSlide: 1,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
